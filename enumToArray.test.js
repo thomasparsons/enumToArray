3224996035d626741dca1dac19b27ac1c30dd712
Tom Parsons <thomaspatrickparsons@gmail.com>
2019-03-27 22:04:40 +0000

    Update enumToArray.test.js

--- enumToArray.test.js
+++ enumToArray.test.js
@@ -4,54 +4,54 @@ describe("when an enum is passed into the function, an array is returned", () =>
   describe("passing in an enum with one value", () => {
     it("array should have one value", () => {
 
-      enum Status {
-        ACTIVE = "active"
+      enum Character {
+        JERRY = "jerry"
       }
 
-      const arr = enumToArray(Status)
+      const arr = enumToArray(Character)
       expect(arr.length).to.equal(1)
-      expect(arr[0]).to.equal("active")
-      expect(arr[0]).to.equal(Status.ACTIVE)
+      expect(arr[0]).to.equal("jerry")
+      expect(arr[0]).to.equal(Character.JERRY)
     })
   })
 
   describe("passing in an enum with two values", () => {
     it("array should have two values", () => {
 
-      enum Status {
-        ACTIVE = "active",
-        DELETED = "deleted"
+      enum Character {
+        JERRY = "jerry",
+        GEORGE = "george"
       }
 
-      const arr = enumToArray(Status)
+      const arr = enumToArray(Character)
       expect(arr.length).to.equal(2)
-      expect(arr[0]).to.equal("active")
-      expect(arr[0]).to.equal(Status.ACTIVE)
+      expect(arr[0]).to.equal("jerry")
+      expect(arr[0]).to.equal(Character.JERRY)
 
-      expect(arr[1]).to.equal("deleted")
-      expect(arr[1]).to.equal(Status.DELETED)
+      expect(arr[1]).to.equal("george")
+      expect(arr[1]).to.equal(Character.GEORGE)
     })
   })
 
   describe("passing in an enum with three values", () => {
     it("array should have three values", () => {
 
-      enum Status {
-        ACTIVE = "active",
-        DELETED = "deleted",
-        INACTIVE = "inactive"
+      enum Character {
+        JERRY = "jerry",
+        GEORGE = "george",
+        KRAMER = "kramer"
       }
 
-      const arr = enumToArray(Status)
+      const arr = enumToArray(Character)
       expect(arr.length).to.equal(3)
-      expect(arr[0]).to.equal("active")
-      expect(arr[0]).to.equal(Status.ACTIVE)
+      expect(arr[0]).to.equal("jerry")
+      expect(arr[0]).to.equal(Character.JERRY)
 
-      expect(arr[1]).to.equal("deleted")
-      expect(arr[1]).to.equal(Status.DELETED)
+      expect(arr[1]).to.equal("george")
+      expect(arr[1]).to.equal(Character.GEORGE)
 
-      expect(arr[2]).to.equal("inactive")
-      expect(arr[2]).to.equal(Status.INACTIVE)
+      expect(arr[2]).to.equal("kramer")
+      expect(arr[2]).to.equal(Character.KRAMER)
     })
   })
 })
